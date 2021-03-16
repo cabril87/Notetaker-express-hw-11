675ad0917932c59dbdfc2b0cb890295bdc018402
Carlos Abril <carlos.abril87@outlook.com>
2021-03-15 20:52:31 -0400

    final

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,12 @@
 const fs = require('fs');
-const jsonData = JSON.parse(fs.readFileSync('db/db.json', "utf8"));
+let jsonData = JSON.parse(fs.readFileSync('db/db.json', "utf8"));
+const { v4: uuidv4 } = require('uuid');
 
 
 module.exports = (app) => {
     
-
-
     app.get('/api/notes', (req, res) => {
         res.send(jsonData);
-
     });
 
     app.get('/api/notes/:id', (req, res) => {
@@ -16,33 +14,35 @@ module.exports = (app) => {
 
     });
     app.post('/api/notes', (req, res) => {
-        let newNote = req.body;
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuidv4()
+        };
         jsonData.push(newNote);
         res.send(newNote.title);
         jsonUpdate(jsonData);
-        
-
-
     });
-
 
     app.delete('/api/notes/:id', (req, res) => {
-        jsonData.splice(req.params.id);
-        jsonUpdate();
-        console.log(req.params.id);
+        let newId;
+        
+        jsonData = jsonData.filter((note) => note.id != req.params.id);
+        console.log(jsonData);
+        for (note of jsonData) {
+            note.id = newId.toString();
+            newId++;
+        }
+        jsonUpdate(jsonData);
+        res.json(jsonData);
     });
 
-    const jsonUpdate = () => {
-    
-    
-        fs.writeFileSync('db/db.json', JSON.stringify(jsonData), err => {
+    const jsonUpdate = (note) => {
+        fs.writeFileSync('db/db.json', JSON.stringify(note), err => {
             if (err) throw err;
             res.json(jsonData);
         })
-    
     }
-
-
 };
 
 
